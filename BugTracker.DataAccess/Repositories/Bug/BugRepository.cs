ca84b47a5a0bb1c6e1c1b40cac58ff9b988a6edb
Samuel Nogalski <nogalskisam@gmail.com>
2019-03-27 22:35:48 +0000

    Add empty bug repository to begin repository pattern

---
+++ BugTracker.DataAccess/Repositories/Bug/BugRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.DataAccess.Repositories
+{
+    public class BugRepository : IBugRepository
+    {
+        public Task GetBugs(bool open)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BugTracker.DataAccess/Repositories/Bug/IBugRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.DataAccess.Repositories
+{
+    public interface IBugRepository
+    {
+        Task GetBugs(bool open);
+    }
+}
