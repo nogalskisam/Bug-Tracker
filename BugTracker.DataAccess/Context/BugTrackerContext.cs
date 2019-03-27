40acc2015f6a644874137cbeb003f8f47a420dc8
Samuel Nogalski <nogalskisam@gmail.com>
2019-03-27 22:09:49 +0000

    Create context class

---
+++ BugTracker.DataAccess/Context/BugTrackerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.DataAccess.Context
+{
+    public class BugTrackerContext : DbContext
+    {
+        public BugTrackerContext(DbContextOptions<BugTrackerContext> options)
+            : base(options)
+        { }
+    }
+}
